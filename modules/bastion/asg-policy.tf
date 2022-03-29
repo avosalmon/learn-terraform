b98f3a7b02bf6b5d622015192b4f79145c08e736
Ryuta Hamasaki <ryuta.hamasaki@accredify.io>
2022-03-29 17:56:08 +0800

    comment out ALBRequestCountPerTarget scaling policy

--- modules/bastion/asg-policy.tf
+++ modules/bastion/asg-policy.tf
@@ -16,18 +16,18 @@ resource "aws_autoscaling_policy" "avg-cpu-policy-greaterthan-50" {
   }
 }
 
-resource "aws_autoscaling_policy" "nlb-target-requests-greaterthan-50" {
-  name                      = "nlb-target-requests-greater-than-50"
-  policy_type               = "TargetTrackingScaling"
-  autoscaling_group_name    = aws_autoscaling_group.asg-group.name
-  estimated_instance_warmup = var.warmup # defaults to ASG default cooldown 300 seconds if not set
+# resource "aws_autoscaling_policy" "nlb-target-requests-greaterthan-50" {
+#   name                      = "nlb-target-requests-greater-than-50"
+#   policy_type               = "TargetTrackingScaling"
+#   autoscaling_group_name    = aws_autoscaling_group.asg-group.name
+#   estimated_instance_warmup = var.warmup # defaults to ASG default cooldown 300 seconds if not set
 
-  # Average CPU Utilization is above 50
-  target_tracking_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ALBRequestCountPerTarget"
-      resource_label         = "${aws_lb.nlb.arn_suffix}/${aws_lb_target_group.nlb-tg.arn_suffix}"
-    }
-    target_value = 50.0 # percent value
-  }
-}
+#   # Average CPU Utilization is above 50
+#   target_tracking_configuration {
+#     predefined_metric_specification {
+#       predefined_metric_type = "ALBRequestCountPerTarget"
+#       resource_label         = "${aws_lb.nlb.arn_suffix}/${aws_lb_target_group.nlb-tg.arn_suffix}"
+#     }
+#     target_value = 50.0 # percent value
+#   }
+# }
